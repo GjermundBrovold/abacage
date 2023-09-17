dcfbf1eee30343c7d88992c4b6c45345cdc5558d
Gjermund <gjermund.brovold@gmail.com>
2023-09-17 22:39:11 +0200

    trying firebase functions

---
+++ src/firebase/database.ts
@@ -0,0 +1,19 @@
+import { getDatabase, ref, set, onValue } from "firebase/database";
+
+function writeUserData(userId, name, email, imageUrl) {
+  const db = getDatabase();
+  set(ref(db, 'users/' + userId), {
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+
+const db = getDatabase();
+const starCountRef = ref(db, 'posts/' + postId + '/starCount');
+onValue(starCountRef, (snapshot) => {
+  const data = snapshot.val();
+  updateStarCount(postElement, data);
+});
+
+export { onValue, writeUserData };
