2b839556c484f7f44abef95f55f2098b598f1f01
perhenrikgithub <perhenrikmv@gmail.com>
2023-09-17 22:11:21 +0200

    m

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,4 +1,65 @@
+<script lang="ts">
+    import default_profile_picture from '$lib/images/default-profile-picture.png'
 
-<div>
-	<h1>User</h1>
+    export let player: {
+		name: string;
+		nickname: string;
+        gamesPLayed: number;
+        sessionsPlayed: number;
+		score: number;
+        pictureUrl: string;
+        isAdmin: boolean;
+	}
+
+    if (player.pictureUrl == "") {
+        player.pictureUrl = default_profile_picture
+    }
+</script>
+
+<style lang="scss">
+    .wrapper {
+        display: grid;
+        place-items: center;
+        width: 100vw;
+        height: 100vh;
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            img {
+                height: 150px;
+                aspect-ratio: 1;
+                margin: 1em;
+            }
+
+            h1 {
+                font-size: 3em;
+                margin: 0;
+            }
+
+            button {
+                height: 3em;
+                border-radius: 1000px;
+                border: 2px solid black;
+
+                :focus {
+                    background: gray;
+                }
+            }
+        }
+    }
+</style>
+
+<div class="wrapper">
+    <div class="container">
+        <img src="{player.pictureUrl}" alt="{player.name}'s profile pic">
+        <h1>{player.nickname}</h1>
+        <h2>{player.name}</h2>
+		<p>adscølakmdfnjkvh</p>
+        {#if player.isAdmin}
+            <button>
+                Create Session
+            </button>
+        {/if}
+    </div>
 </div>
