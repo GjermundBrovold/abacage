ee465d8a8a1845185d74261db98de548f66aca9c
Kasper <kasper.mustad.juell@linux.com>
2023-09-18 11:32:44 +0200

    moved to selectedPlayers

--- src/routes/admin/selectedPlayers.ts
+++ src/routes/admin/selectedPlayers.ts
@@ -12,7 +12,7 @@ export const selectedPlayers:Array<typeof player> = []
 export function updatePlayer(p: typeof player){
 	if (selectedPlayers.indexOf(p) != -1) {
 		console.log("removed: " + p.name);
-		selectedPlayers.splice(selectedPlayers.indexOf(p), 1)
+		//selectedPlayers.splice(selectedPlayers.indexOf(p), 1)
 	} else {
 		console.log("added: " + p.name);
 		selectedPlayers.push(p)
