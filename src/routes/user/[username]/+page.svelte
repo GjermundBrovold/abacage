c0c377e87fc219197e4f0959f186eeac1fe47c07
Kasper <kasper.mustad.juell@linux.com>
2023-09-23 13:10:23 +0200

    added user routing, can now access user in [username] folder

--- src/routes/user/[username]/+page.svelte
+++ src/routes/user/[username]/+page.svelte
@@ -1,6 +1,11 @@
 <!-- Vise spesifikk bruker -->
 <script lang='ts'>
     import type { playerInterface } from '../../player';
-    export let player: playerInterface;
-    console.log(player);
+
+    // don't change name; it won't work ¯\_(ツ)_/¯
+    export let data: playerInterface;
 </script>
+
+<div>
+    {data.name}
+</div>
