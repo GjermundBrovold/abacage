98890cfb4fb8e4dca7129cdff15b624eb390e971
Kasper <kasper.mustad.juell@linux.com>
2024-01-03 20:51:54 +0100

    Removed unused function

--- src/routes/sessions/components/Session.svelte
+++ src/routes/sessions/components/Session.svelte
@@ -4,25 +4,6 @@
     export let scores: number[];
     let players = session.players;
 
-	function calcScore(name: string) {
-        let score = 0
-
-        session.matches.forEach(match => {
-            let index = match.players[0].indexOf(name)
-            if (index >= 0){
-                score += match.team1Points
-            }
-            else if (match.team1Points == 0){
-                score += 3
-            }
-            else if (match.team1Points == 1){
-                score += 1
-            }
-        })
-
-        return score
-	}
-
 </script>
 
 <div>
